d2b2bfe761d503a8f50258eb58883cc1a5db1827
aashish <aashisharukonda@gmail.com>
2024-01-28 12:04:05 +0530

    build and prod mode

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent=React.createElement("div",{id:"parent"},
     React.createElement("h1",{id:"child"},"child")
 )
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("h1", {
+    id: "child"
+}, "child"));
+const heading = React.createElement("h1", {
+    "id": "heading"
+}, "hello");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+root.render(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
